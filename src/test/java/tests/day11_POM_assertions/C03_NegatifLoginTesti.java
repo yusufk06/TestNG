c37333bda3721a6539b200ab6c7b12fb16a91b19
“yusufk06” <yusuf-9207@outlook.com>
2023-07-09 19:59:28 +0300

    v1

---
+++ src/test/java/tests/day11_POM_assertions/C03_NegatifLoginTesti.java
@@ -0,0 +1,70 @@
+package tests.day11_POM_assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QdPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C03_NegatifLoginTesti {
+
+    // qd anasayfaya gidin
+    // 3 test method u olusturun ve
+    // asagidaki durumlarda giris yapilamadigini test edin
+    //1- gecerli username, gecersiz password
+    //2- gecersiz username,gecerli password
+    //3- gecersiz username,gecersiz password
+
+    QdPage qdPage= new QdPage();
+    @Test(groups = "smoke")
+    public void negatifLogin1(){
+        ReusableMethods.bekle(3);
+        qdPage= new QdPage();
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        qdPage.ilkLoginLinki.click();
+         qdPage.cookieTik.click();
+        ReusableMethods.bekle(2);
+
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecerliUsername"));
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecersizPassword"));
+        qdPage.LoginKutusu.click();
+
+        Assert.assertTrue(qdPage.LoginKutusu.isEnabled());
+        Driver.closeDriver();
+
+    }
+
+
+    @Test(groups = {"smoke","mr1"})
+    public void negatifLogin2(){
+        qdPage= new QdPage();
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        qdPage.ilkLoginLinki.click();
+       // qdPage.cookieTik.click();
+        ReusableMethods.bekle(2);
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecersizUsername"));
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecerliPassword"));
+        qdPage.LoginKutusu.click();
+
+        Assert.assertTrue(qdPage.LoginKutusu.isEnabled());
+        Driver.closeDriver();
+
+    }
+
+    @Test
+    public void negatifLogin3(){
+        qdPage= new QdPage();
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        qdPage.ilkLoginLinki.click();
+       // qdPage.cookieTik.click();
+        ReusableMethods.bekle(2);
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecersizUsername"));
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecersizPassword"));
+        qdPage.LoginKutusu.click();
+
+        Assert.assertTrue(qdPage.LoginKutusu.isEnabled());
+        Driver.closeDriver();
+
+    }
+}
